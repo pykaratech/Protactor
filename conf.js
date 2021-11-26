49343074b16121205ec6ec3cc563bebd1e474403
pykaratech <50095081+pykaratech@users.noreply.github.com>
2021-11-26 16:57:24 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 const { percySnapshot } = require('@percy/protractor')
 describe('Integration test with visual testing', function () {
     it('Loads the app', function () {
-        browser.get('http://example.com')
+        browser.get('https://localhost:44323/')
         percySnapshot('My snapshot')
     })
 })
